fbaf2b9fbbf42d362c2ca7600c94bf5266324143
CarlOlson <CarlOlson@users.noreply.github.com>
2021-05-17 15:03:26 +0900

    remove fs/path usage in serverSideTranslations

--- src/serverSideTranslations.ts
+++ src/serverSideTranslations.ts
@@ -1,26 +1,15 @@
-import fs from 'fs'
-import path from 'path'
-
 import { createConfig } from './config/createConfig'
 import createClient from './createClient'
 
 import { UserConfig, SSRConfig } from './types'
 
-const DEFAULT_CONFIG_PATH = './next-i18next.config.js'
-
 export const serverSideTranslations = async (
   initialLocale: string,
   namespacesRequired: string[] = [],
-  configOverride: UserConfig | null = null,
+  userConfig: UserConfig | null = null,
 ): Promise<SSRConfig> => {
   if (typeof initialLocale !== 'string') {
     throw new Error('Initial locale argument was not passed into serverSideTranslations')
-  }
-
-  let userConfig = configOverride
-
-  if (fs.existsSync(path.resolve(DEFAULT_CONFIG_PATH))) {
-    userConfig = await import(path.resolve(DEFAULT_CONFIG_PATH))
   }
 
   if (userConfig === null) {
@@ -31,12 +20,6 @@ export const serverSideTranslations = async (
     ...userConfig,
     lng: initialLocale,
   })
-
-  const {
-    defaultLocale,
-    localeExtension,
-    localePath,
-  } = config
 
   const { i18n, initPromise } = createClient({
     ...config,
@@ -50,14 +33,6 @@ export const serverSideTranslations = async (
   config.locales.forEach(lng => {
     initialI18nStore[lng] = {}
   })
-
-  if (namespacesRequired.length === 0) {
-    const getAllNamespaces = (path: string) =>
-      fs.readdirSync(path)
-        .map(file => file.replace(`.${localeExtension}`, ''))
-
-    namespacesRequired = getAllNamespaces(path.resolve(process.cwd(), `${localePath}/${defaultLocale}`))
-  }
 
   namespacesRequired.forEach((ns) => {
     for (const locale in initialI18nStore) {
